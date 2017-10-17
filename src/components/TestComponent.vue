8d4f65bb661eb3aebc8af7feea0f2ed232811d01
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2017-10-17 11:40:14 -0400

    Create TestComponent.vue

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+    <div>{{ test }}</div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            test: 'This is a test component'
+        }
+    }
+}
+</script>
+
+<style lang="css">
+</style>
