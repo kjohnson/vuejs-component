f4e4da1808ba87518c02623f5e35eadd1918f228
Kyle B. Johnson <kjohnson@users.noreply.github.com>
2017-10-17 11:39:43 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import TestComponent from './components/TestComponent.vue';
+
 export default {
+
+    // When used as a plugin, automatically register each component.
     install(Vue) {
-        // Vue.component('tag-name', Component);
+        Vue.component( 'test-component', TestComponent );
     },
 };
+
+// Export each component individually for manual registration.
+export { TestComponent };
